670095cec55a44c1c7c6d2206dc435b96c697f12
Walter Cheng <chengwaltj@gmail.com>
2025-03-08 14:33:12 -0800

    parses packets up to reading the actual data

--- udp_response.py
+++ udp_response.py
@@ -4,38 +4,98 @@ import sys
 import json
 import re
 import warnings
-
-# read config.json
-with open("universalproto/config.jsonc") as c:
-    # zander gave me this regex, goat
-    real_json = re.sub(r"(\/\/.*?\n|\/\*.*?\*\/)", r"", c.read(), flags=re.DOTALL) # remove all comments from config.jsonc
-    config = json.loads(real_json)
-
-# flip deviceIDs and put into boards dict
-boards = {}
-for board, id in config['deviceIds'].items():
-    boards.update({str(id): board})
+import fnmatch
+import pprint
 
 class Packet:
     def __init__(self):
-        self.fields = dict()
+        self.fields = {}
         self.timestamp = 0
         self.error = True
         self.board = ""
         self.id = -1
 
-# under construction, parse packet data
-def parsePacket(data, addr):
-    packet = Packet()
-    boardID = addr[0][-2:]
+class Config:
+    def __init__(self):
+        self.boards = {}
+        self.ip_lookup = {}
+        self.packets = {}
 
-    if boardID not in boards: # confirm packet board id
+config = Config()
+
+# idk https://en.wikipedia.org/wiki/Fletcher%27s_checksum
+def fletcher16(data):
+	a = 0
+	b = 0
+	for i in range(len(data)):
+		a = (a + data[i]) % 256
+		b = (b + a) % 256
+	return a | (b << 8)
+
+# read config.jsonc
+with open("universalproto/config.jsonc") as c:
+    # zander gave me this regex, goat
+    real_json = re.sub(r"(\/\/.*?\n|\/\*.*?\*\/)", r"", c.read(), flags=re.DOTALL) # remove all comments from config.jsonc
+    config_json = json.loads(real_json)
+
+# read packets.jsonc
+with open("universalproto/packets.jsonc") as p:
+    real_json = re.sub(r"(\/\/.*?\n|\/\*.*?\*\/)", r"", p.read(), flags=re.DOTALL) # remove all comments from config.jsonc
+    packets_json = json.loads(real_json)
+
+# flip deviceIDs and put into config.boards
+for board, id in config_json["deviceIds"].items():
+    config.boards.update({str(id): board})
+    config.ip_lookup.update({board: str(id)})
+    config.packets[board] = {}
+        
+# add packet ids to config.packets
+for packet in packets_json:
+    for writer in packet["writes"]:
+        for board_name in config.ip_lookup:
+            if fnmatch.fnmatch(board_name, writer):
+                if "payload" in packet:
+                    config.packets[board_name].update({str(packet["id"]): packet["payload"]})
+
+# under construction, parse packet data
+# ref: struct.unpack_from() https://docs.python.org/3/library/struct.html
+def parse_packet(data, addr):
+    packet = Packet()
+
+    # parse packet data into variables
+    boardID = addr[0][-2:]
+    unpack = struct.unpack_from("<BBIH", data[0:8])
+    id = str(unpack[0]) # from what i have seen id should be a str
+    length = unpack[1]
+    run_time = unpack[2]
+    checksum = unpack[3]
+
+    # add board name to packet
+    if boardID not in config.boards: # confirm packet board id
         warnings.warn(f"Warning: IP {addr[0]} not recognized")
         packet.error = True
         return packet
-
-    board = boards[boardID]
+    
+    board = config.boards[boardID]
     packet.board = board
+    
+    # parse actual data and calculate checksum
+    unpack = struct.unpack_from(f"<{6}sH{length}s", data)
+    field_data = unpack[2]
+    expected_checksum = fletcher16(unpack[0] + unpack[2])
+    
+    # confirm checksum validity
+    if not expected_checksum == checksum:
+        warnings.warn(f"Warning: Invalid checksum from {board} (packet id {id})")
+        packet.error = True
+        return packet
+
+    # confirm packet id validity
+    if not id in config.packets[board]:
+        warnings.warn(f"Warning: Unrecognized packet {id} on {board}")
+        print(config.packets[board])
+        packet.error = True
+        return packet
 
     return 0
 
@@ -75,5 +135,6 @@ while True:
     # print("bobr")
 
     data, addr = mc_sock.recvfrom(1024)
-    parsePacket(data, addr)
-    #print(f"Received multicast packet from {addr[0]}:{addr[1]}: " + data.hex(" ") + '\n')
+    # print(f"Received multicast packet from {addr[0]}:{addr[1]}: " + data.hex(" ") + '\n')
+    parse_packet(data, addr)
+
